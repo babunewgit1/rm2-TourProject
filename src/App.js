ce22cbd2f07c201c788ac8f5c4e0bcd9807a766d
babunewgit1 <babunewx11@gmail.com>
2023-02-01 17:59:20 +0600

    plate

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <p>react boilar plate</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
